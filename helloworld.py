9eb5419196a238f60cdd3260ceb0ff3f363fcdba
dev-yoga <spin_shoppe@icloud.com>
2022-03-10 11:29:25 -0800

    Practice minutes

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print('Hello, World!')
+
+person = input('What is your name?')
+
+print('Hello,', person,'!')
+
---
+++ practicehours.py
@@ -0,0 +1,3 @@
+practice_minutes = input('How many practice minutes?')
+
+print('You have', practice_minutes, 'practice minutes.')
