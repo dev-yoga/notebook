68a26f833438f35a75e2fa1eca173583b24e106a
dev-yoga <spin_shoppe@icloud.com>
2022-03-10 11:35:34 -0800

    Operators and such ?

--- practicehours.py
+++ practicehours.py
@@ -1,3 +1,9 @@
 practice_minutes = input('How many practice minutes?')
 
 print('You have', practice_minutes, 'practice minutes.')
+
+practice_hours = (practice_minutes / 2)
+
+hours_remaining = (50 - practice_hours) 
+
+print('You have', practice_hours, 'practice hours and you require', hours_remaining, 'to complete your training requirements.')
