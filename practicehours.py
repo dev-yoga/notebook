554c1842396c7198b11dd0cb7cb068b9da31a79b
dev-yoga <spin_shoppe@icloud.com>
2022-03-11 17:02:45 -0800

    Getting there

--- practicehours.py
+++ practicehours.py
@@ -2,16 +2,10 @@ practice_minutes = input('How many practice minutes?')
 
 print('You have', practice_minutes, 'practice minutes.')
 
-print(practice_minutes)
+practice_hours = (int(practice_minutes) / 60)
 
-print(int(practice_minutes) / 2)
+hours_remaining = (50 - int(practice_hours)) 
 
-# practice_hours = (practice_minutes / 2)
-
-# print(practice_hours / practice_minutes)
-
-# hours_remaining = (50 - practice_hours) 
-
-# print(hours_remaining)
+print(hours_remaining)
 
 # print('You have', practice_hours, 'practice hours and you require', hours_remaining, 'to complete your training requirements.')
