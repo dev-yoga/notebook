e9efe66120e47dfd18172eff02591a895d49695b
dev-yoga <spin_shoppe@icloud.com>
2022-03-11 16:57:20 -0800

    Upstream?

--- practicehours.py
+++ practicehours.py
@@ -2,8 +2,16 @@ practice_minutes = input('How many practice minutes?')
 
 print('You have', practice_minutes, 'practice minutes.')
 
-practice_hours = (practice_minutes / 2)
+print(practice_minutes)
 
-hours_remaining = (50 - practice_hours) 
+print(practice_minutes / 2)
 
-print('You have', practice_hours, 'practice hours and you require', hours_remaining, 'to complete your training requirements.')
+# practice_hours = (practice_minutes / 2)
+
+# print(practice_hours / practice_minutes)
+
+# hours_remaining = (50 - practice_hours) 
+
+# print(hours_remaining)
+
+# print('You have', practice_hours, 'practice hours and you require', hours_remaining, 'to complete your training requirements.')
